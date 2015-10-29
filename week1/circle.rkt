ed4e0da67d0ff6a3dadee6d72ba4cfd4e3c621e3
AKarakehayova <alexzk95@gmail.com>
2015-10-30 01:43:12 +0200

    Create circle.rkt

---
+++ week1/circle.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+(define (circle? circle-x circle-y radius point-x point-y)
+(if (<= (+ (* (- point-x circle-x) (- point-x circle-x)) (* (- point-y circle-y) (- point-y circle-y))) (* radius radius))
+    #t
+    #f)
+  )
