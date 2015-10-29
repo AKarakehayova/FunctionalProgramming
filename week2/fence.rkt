770765cc77a46fe24e248af39234951b6aa564d4
AKarakehayova <alexzk95@gmail.com>
2015-10-30 01:39:15 +0200

    Create fence.rkt

---
+++ week2/fence.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(define (string-repeat str n)
+  (define (helper i new)
+    (if (>= i n) new
+        (helper (+ i 1) (string-append new str))
+        )
+    )
+  (helper 1 str)
+  )
+
+(define(fence n)
+ (define(helper start fence l n m end)
+   (string-append start fence l n m fence end)
+   )
+(helper "{" (string-repeat "-" (round (+ 1 (log n)))) ">" (number->string n) "<" "}")
+  )
