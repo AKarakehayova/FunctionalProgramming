af4531a59c88a366b04ef6055d1a75bd71e9b1f8
AKarakehayova <alexzk95@gmail.com>
2016-01-22 16:47:44 +0200

    Delete containsDigits.hs

--- week10/containsDigits.hs
+++
@@ -1,7 +0,0 @@
-containsDigits :: Int -> Int -> Bool
-containsDigits x y = containsDigits' (show x) (show y)
-	where 
-		containsDigits' _ [] = True
-		containsDigits' x (y:ys) 
-			|elem y x = containsDigits' x ys
-			|otherwise = False
